9e20a7ebb3114afe177c3d652eae51fee3606d97
Bix <lasdo2@outlook.com>
2023-11-14 10:27:59 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+console.log("Loaded.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let historico = []
+//
+//
+//$("#operar").click(function(){
+//	var value1 =parseInt($("#value1").val());
+//	var value2 =parseInt($("#value2").val());
+//
+//
+//	operacao = $("#selecionar").val();
+//	if (operacao == "Somar"){
+//		resultado = value1+value2
+//	}
+//	if (operacao == "Subtrair"){
+//		resultado = value1-value2
+//	}
+//	if (operacao == "Multiplicar"){
+//		resultado = value1*value2
+//	}
+//	if (operacao == "Dividir"){
+//		resultado = value1/value2
+//	}
+//
+//	if (!isNaN(resultado)){
+//		console.log(historico)
+//		historico.push(resultado)
+//		console.log(historico.length)
+//		if (historico.length <= 10){
+//			$("#resultado").html(resultado)
+//			$("#historico").html("Histórico de resultados:"+historico+",")
+//		} else {
+//			console.log(historico)
+//			historico.shift()
+//			$("#historico").html("Histórico de resultados:"+historico)
+//		}
+//	}
+//});
+
