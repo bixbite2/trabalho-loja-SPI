472457864ebaafb4e44c3bc83fd25300b1a6a543
victor-grandmasterwitcher <victortavamaral@gmail.com>
2023-12-11 18:37:50 -0300

    finalizado ?

--- js/main.js
+++ js/main.js
@@ -1,18 +1,43 @@
 console.log("Loaded.")
 
-console.log(pega_livro())
+const books = [
+    {
+        name: "Trilogia Fundação",
+        slang: "fundacao",
+        price: 119.90,
+        cover: "background-image: url(public/livros/fundacao.jpg)",
+    },
+    {
+        name: "O Hobbit",
+        slang: "hobbit",
+        price: 59.90,
+        cover: "background-image: url(public/livros/hobbit.jpg)",
+    },
+    {
+        name: "1984",
+        slang: "1984",
+        price: 23.50,
+        cover: "background-image: url(public/livros/1984.jpg)",
+    },
+    {
+        name: "All the Tomorrows",
+        slang: "altomorrows",
+        price: 73.55,
+        cover: "background-image: url(public/livros/altomorrows.jpg)",
+    },
+    {
+        name: "Hamlet",
+        slang: "hamlet",
+        price: 23.55,
+        cover: "background-image: url(public/livros/hamlet.jpg)",
+    },
+]
 
-//<div class="book">
-//    <div class="cover" style="background-image: url(public/livros/hobbit.jpg);"></div>
-//    <div class="name">O Hobbit</div>
-//    <div class="price">R$ 59,90</div>
-//    <button type="button" onclick="goToCart('hobbit')" class="button">Comprar</button>
-//</div>
-
-
-
-
-
+let get_livro_info = (id) => {
+    let book = books.find(item => item.slang == id)
+    if(book) return book
+    else return null
+}
 
 let cria_livro = (id) => {
     let livro = document.createElement('div')
@@ -47,7 +72,6 @@ let cria_livro = (id) => {
                 nome.appendChild(document.createTextNode("Hamlet"));
                 preco.appendChild(document.createTextNode("R$ 23,55"));
             break;
-  
     }
     
     livro.appendChild(capa)
@@ -56,61 +80,3 @@ let cria_livro = (id) => {
 
     return livro
 }
-
-let olivro = cria_livro(pega_livro())
-
-$('#cart').append(olivro)
-
-
-// Como pegar os dados da url
-// let params = new URLSearchParams(document.location.search);
-// let name = params.get("name"); // is the string "Jonathan"
-// let age = parseInt(params.get("age"), 10); // is the number 18
-
-
-//<div class={book}>
-//    <div class={cover} style='background-image: url(public/livros/{book}.jpg);'></div>
-//    <div class='name'>{book}</div>
-//    <div class='price'>R$ {book_price}</div>
-//</div>
-
-
-
-
-//let historico = []
-//
-//
-//$("#operar").click(function(){
-//	var value1 =parseInt($("#value1").val());
-//	var value2 =parseInt($("#value2").val());
-//
-//
-//	operacao = $("#selecionar").val();
-//	if (operacao == "Somar"){
-//		resultado = value1+value2
-//	}
-//	if (operacao == "Subtrair"){
-//		resultado = value1-value2
-//	}
-//	if (operacao == "Multiplicar"){
-//		resultado = value1*value2
-//	}
-//	if (operacao == "Dividir"){
-//		resultado = value1/value2
-//	}
-//
-//	if (!isNaN(resultado)){
-//		console.log(historico)
-//		historico.push(resultado)
-//		console.log(historico.length)
-//		if (historico.length <= 10){
-//			$("#resultado").html(resultado)
-//			$("#historico").html("Histórico de resultados:"+historico+",")
-//		} else {
-//			console.log(historico)
-//			historico.shift()
-//			$("#historico").html("Histórico de resultados:"+historico)
-//		}
-//	}
-//});
-
